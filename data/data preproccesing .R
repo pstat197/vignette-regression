843c3a28eac234900537aa71dc2eef22c8632075
Alekscichosz <114379239+alekscichosz@users.noreply.github.com>
2022-11-28 18:01:39 -0800

    Add some of the files we need, did a little bit of data cleaning. The professor uses a nifty way to do training and testing split so I need to look that up.

---
+++ data/data preproccesing .R
@@ -0,0 +1,19 @@
+require(readr)
+require(dplyr)
+require(fastDummies)
+require(stringi)
+
+
+life_data <- read_csv("data/life-expectancy-raw.csv")
+life_data %>%
+  group_by(Country) %>%
+  summarise(n = n())
+
+# There are 193 countries, and I assume each country has 16 entries
+
+life_data <- life_data %>%
+  dummy_cols('Status',remove_first_dummy = TRUE, remove_selected_columns = TRUE)
+
+
+life_clean <- life_data
+write_csv(life_clean,"data/life_clean.csv")
