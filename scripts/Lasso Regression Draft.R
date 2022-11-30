161009511bb9b1912f29318c8fb2a90c2d8117ec
justinv0 <91296852+justinv0@users.noreply.github.com>
2022-11-30 13:55:01 -0800

    Main vignette file and lasso regression file

--- scripts/Lasso Regression Draft.R
+++ scripts/Lasso Regression Draft.R
@@ -1,1 +1,76 @@
-# Lasso Regression
+# LASSO Regression
+
+# Loading package
+library(dplyr)
+library(glmnet)
+library(caret)
+library(tidyverse)
+library(tidymodels)
+# Loading Data
+
+# load cleaned data
+# Loading Data
+life_clean <- read.csv("data/life_clean.csv")
+
+# Cleaning data
+life_clean <- life_clean %>%
+  na.omit()
+
+eval_results <- function(true, predicted, df) {
+  SSE <- sum((predicted - true)^2)
+  SST <- sum((true - mean(true))^2)
+  R_square <- 1 - SSE / SST
+  RMSE = sqrt(SSE/nrow(df))
+  
+  # Model performance metrics
+  data.frame(
+    RMSE = RMSE,
+    Rsquare = R_square)
+}
+# Model Building : LASSO Regression
+
+# for reproducibility
+set.seed(12)
+
+# partition data
+partitions <- life_clean %>%
+  initial_split(prop = 0.8)
+
+# Training Set
+train_x <- training(partitions) %>%
+  select(-Life.expectancy) %>%
+  as.matrix()
+train_y <- training(partitions) %>%
+  pull(Life.expectancy)
+
+# Testing Set
+test_x <- testing(partitions) %>%
+  select(-Life.expectancy) %>%
+  as.matrix()
+test_y <- testing(partitions) %>%
+  pull(Life.expectancy)
+
+# examine
+lambdas <- 10^seq(2, -3, by = -.1)
+
+# Perform cross validaton to find best value of lambda
+lasso_reg <- cv.glmnet(train_x, train_y, alpha = 1, lambda = lambdas)
+
+# Best lambda value
+best_lambda <- lasso_reg$lambda.min
+
+lasso_model <- glmnet(train_x, train_y, alpha = 1, lambda = best_lambda, standardize = TRUE)
+
+predictions_train <- predict(lasso_model, s = best_lambda, newx = train_x)
+
+eval_results(train_y, predictions_train, training(partitions))
+
+predictions_test <- predict(lasso_model, s = best_lambda, newx = test_x)
+
+eval_results(test_y, predictions_test, testing(partitions))
+)
+
+
+
+
+
